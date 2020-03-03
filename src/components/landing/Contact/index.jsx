6f7b60982bd7b81d949c83475f02278d226284ca
jaysonnabor <sonabstudios@gmail.com>
2020-03-03 20:46:07 +0800

    added contact me header

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -7,10 +7,12 @@ import ContactForm from './ContactForm'
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
+    <h2>Contact Me</h2>
       <ContactForm />
     </Details>
     <Thumbnail>
       <img src={contact} alt="I’m Jayson and I’m a Software Engineer!" />
     </Thumbnail>
+
   </Wrapper>
 )
