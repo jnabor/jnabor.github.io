05bd06b31c5a49f1bfbcf58c436bd370e2c9bbbd
jnabor <sonabstudios@gmail.com>
2020-07-22 16:26:33 +0800

    updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,6 @@ import { Layout, SEO } from 'components/common';
 export default () => (
   <Layout>
     <SEO title="404: Not found" location="/404" />
-    <div style={{ padding: '20px' }}><p>You've just hit a route that doesn&#39;t exist.</p></div>
+    <div style={{ textAlign: 'center', height: `calc(100vh - 422px)`}}><h2 style={{ marginTop: '80px'}}>You've just hit a route that doesn&#39;t exist.</h2></div>
   </Layout>
 );
