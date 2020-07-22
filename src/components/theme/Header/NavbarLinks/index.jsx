6827bc85589aa7a2acd43eeb8dbc9459f77fb804
jnabor <sonabstudios@gmail.com>
2020-07-22 16:02:19 +0800

    hav bar active indicator

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -1,39 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import homeIcon from '../../../../assets/illustrations/home.svg'
 import { Wrapper, HomeIcon, NavButton } from './styles'
 
-const NavbarLinks = () => (
+const NavbarLinks = () => {
+  const [home, setHome] = useState(true)
+  const [project, setProject] = useState(false)
+  const [skills, setSkills] = useState(false)
+  const [contact, setContact] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const handleScroll = () => {
+    setHome(window.scrollY < 200 && window.scrollY > -1)
+    setProject(window.scrollY < 1200 && window.scrollY >= 200)
+    setSkills(window.scrollY < 3000 && window.scrollY >= 1200)
+    setContact(window.scrollY >= 3000)
+    console.log(window.scrollY)
+  }
+
+
+  return (
   <Wrapper >
     <div>
       <AnchorLink href="#home" style={{ color: '#EFEFEF' }}>
-        <HomeIcon current>
+        <HomeIcon current={home}>
           <img src={homeIcon} alt="home" style={{ padding: '0', margin: '0', height: '24px', width: '24px'}}/>
         </HomeIcon>
       </AnchorLink>
     </div>
     <div>
       <AnchorLink href="#projects" style={{ color: '#EFEFEF' }}>
-        <NavButton>
+        <NavButton current={project}>
             PROJECTS
         </NavButton>        
       </AnchorLink>
     </div>
     <div>
       <AnchorLink href="#about" style={{ color: '#EFEFEF' }}>
-      <NavButton>
+      <NavButton current={skills}>
         SKILLS
         </NavButton> 
       </AnchorLink>
     </div>
     <div>
       <AnchorLink href="#contact" style={{ color: '#EFEFEF' }}>
-      <NavButton>
+      <NavButton current={contact}>
         CONTACT
         </NavButton> 
       </AnchorLink>
     </div>
-  </Wrapper>
-)
+  </Wrapper>)
+}
 
 export default NavbarLinks
