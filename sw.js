a3470d79317da461c6bc8f3ee397c99e69064b6c
jnabor <sonabstudios@gmail.com>
2020-07-22 22:58:07 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-614a0832488ba81db057.js"
+    "url": "webpack-runtime-aa4930d331a243e0fe1f.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "adce65ada9c2ec71dfe8cd1f55cdaf59"
+    "revision": "f70c10b9919a1838ee5b3f5fbc0b4d55"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "3a5c4d7b83fcbbd1bb58cfd1b27863cb"
+    "revision": "0ea2347c89067f0c0aa908ed5196b964"
   },
   {
     "url": "polyfill-d06486d78b0d3a46b835.js"
